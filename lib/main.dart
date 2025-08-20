15051b20ffd2afa9ea215a525f7b5a1c1139c11c
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-08-20 10:47:41 +0100

    feat: Integrate GoRouter for navigation
    This commit replaces the direct instantiation of the `Signup` page in `main.dart` with a `MaterialApp.router` configuration.
    
    - The `routerConfig` is now set to `router` (presumably defined in `app_routes.dart`), enabling declarative routing.
    - The import for `Signup` page is removed and replaced with an import for `app_routes.dart`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/app/ui/pages/auth/signup/signup.dart';
+import 'package:frontend/app/routes/app_routes.dart';
 import 'package:frontend/app/ui/themes/theme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(MaterialApp(home: Signup(), theme: primaryTheme));
+  runApp(MaterialApp.router(routerConfig: router, theme: primaryTheme));
 }
