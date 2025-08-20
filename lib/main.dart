6881166ef2343fdedfe366cb29cc8c067a6cd995
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-08-20 08:12:36 +0100

    feat: Update main to launch Signup screen
    This commit modifies `main.dart` to:
    - Initialize `WidgetsFlutterBinding`.
    - Change the initial route from `SplashScreen` to `Signup`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/app/ui/pages/splashScreen/splash_screen.dart';
+import 'package:frontend/app/ui/pages/auth/signup/signup.dart';
 import 'package:frontend/app/ui/themes/theme.dart';
 
 void main() {
-  runApp(MaterialApp(home: const SplashScreen(), theme: primaryTheme));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MaterialApp(home: Signup(), theme: primaryTheme));
 }
