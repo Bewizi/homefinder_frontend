96ffee0ecfd99c934450442825f2dd3d7a259e45
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2025-08-27 12:17:49 +0100

    feat: Integrate PropertyProvider using MultiProvider
    This commit updates `main.dart` to integrate `PropertyProvider` into the application using `MultiProvider`.
    
    - Imports `property_provider.dart` and `provider.dart`.
    - Wraps `MaterialApp.router` with `MultiProvider`.
    - Adds `PropertyProvider` to the list of providers.
    - Sets `debugShowCheckedModeBanner` to `false` in `MaterialApp.router`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:homefinder/app/provider/property_provider.dart';
 import 'package:homefinder/app/routes/app_routes.dart';
 import 'package:homefinder/app/ui/themes/theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(MaterialApp.router(routerConfig: router, theme: primaryTheme));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => PropertyProvider()),
+      ],
+      child: MaterialApp.router(
+        routerConfig: router,
+        theme: primaryTheme,
+        debugShowCheckedModeBanner: false,
+      ),
+    ),
+  );
 }
