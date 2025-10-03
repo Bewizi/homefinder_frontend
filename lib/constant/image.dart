e38d7c1ec10410196db73a21bc369c0b2eaec0b2
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-10-03 15:18:32 +0100

    feat: Add AppImages and AppSvgs classes for image and SVG asset management

---
+++ lib/constant/image.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const String logo = 'assets/images/logo.png';
+  static const String placeholder = 'assets/images/placeholder.png';
+  static const String background = 'assets/images/background.jpg';
+}
---
+++ lib/constant/svgs.dart
@@ -0,0 +1,6 @@
+class AppSvgs {
+  static const String kAmicoOne = 'assets/svgs/amico_one.svg';
+  static const String kAmicoTwo = 'assets/svgs/amico_two.svg';
+  static const String kAmicoThree = 'assets/svgs/amico_three.svg';
+  static const String settings = 'assets/svgs/settings.svg';
+}
