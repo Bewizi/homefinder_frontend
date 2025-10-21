917806e2e00f969672a672f59f4b0afedb73c827
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-10-21 11:59:53 +0100

    feat: Define AppColors class with a set of predefined color constants

---
+++ lib/core/presentation/themes/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class AppColors {
+  static final Color kBrand50 = Color(0xFF4F46E5);
+  static final Color kJet = Color(0xFF2C2C2C);
+  static final Color kWhite = Color(0xFFFFFFFF);
+  static final Color kBlack = Color(0xFF000000);
+  static final Color kGreyBg = Color(0xFFFBFDFF);
+  static final Color kGrey30 = Color(0xFF7A7A7A);
+  static final Color kGrey5 = Color(0xFFEAEAEA);
+  static final Color kBattleshipGray = Color(0xFF898989);
+  static final Color kRed = Color(0xFFFF0000);
+  static final Color kGrey20 = Color(0xFFA4A4A4);
+  static final Color kTransparent = Color(0x00000000);
+  static final Color kEerieBlack = Color(0xFF171717);
+}
