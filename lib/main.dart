f9e978615f351442a19fc11525ddeca97802f4a1
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-10-21 11:59:44 +0100

    feat: Integrate ScreenUtil for responsive UI and refactor main app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:homefinder/app/provider/property_provider.dart';
-import 'package:homefinder/app/routes/app_routes.dart';
-import 'package:homefinder/app/ui/themes/theme.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:homefinder/core/presentation/navigation/routes.dart';
+import 'package:homefinder/core/presentation/themes/themes.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => PropertyProvider()),
-      ],
-      child: MaterialApp.router(
-        routerConfig: router,
-        theme: primaryTheme,
-        debugShowCheckedModeBanner: false,
-      ),
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          routerConfig: router,
+          theme: AppThemeData.lightTheme(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
+  }
 }
