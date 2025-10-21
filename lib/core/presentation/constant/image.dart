c91b6608840ecf263a80d1720f8ee51b642f1cc1
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-10-21 11:59:07 +0100

    feat: Add constants for logo and image effects

--- lib/core/presentation/constant/image.dart
+++ lib/core/presentation/constant/image.dart
@@ -2,4 +2,5 @@ class AppImages {
   static const String logo = 'assets/images/logo.png';
   static const String placeholder = 'assets/images/placeholder.png';
   static const String background = 'assets/images/background.jpg';
+  static const String kImageFx = 'assets/images/image_fx.png';
 }
--- lib/core/presentation/constant/svgs.dart
+++ lib/core/presentation/constant/svgs.dart
@@ -3,4 +3,5 @@ class AppSvgs {
   static const String kAmicoTwo = 'assets/svgs/amico_two.svg';
   static const String kAmicoThree = 'assets/svgs/amico_three.svg';
   static const String settings = 'assets/svgs/settings.svg';
+  static const String kLogo = 'assets/svgs/home_search.svg';
 }
