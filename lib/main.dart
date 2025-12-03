e5f4b3e560ecf632114ee17ddc4e98555d14d764
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-12-03 18:50:02 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:homefinder/core/presentation/navigation/routes.dart';
@@ -6,7 +8,7 @@ import 'package:homefinder/core/presentation/themes/themes.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(MyApp());
+  runApp(DevicePreview(enabled: !kReleaseMode, builder: (context) => MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,6 +22,9 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) {
         return MaterialApp.router(
+          locale: DevicePreview.locale(context),
+          builder: DevicePreview.appBuilder,
+          // useInheritedMediaQuery: true,
           routerConfig: router,
           theme: AppThemeData.lightTheme(),
           debugShowCheckedModeBanner: false,
