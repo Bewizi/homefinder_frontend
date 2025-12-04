cc889bec429ca382f6e6967c7e1c56dedf31b051
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-12-04 14:51:01 +0100

    Update image.dart

--- lib/core/presentation/constant/image.dart
+++ lib/core/presentation/constant/image.dart
@@ -5,4 +5,7 @@ class AppImages {
   static const String kImageFx = 'assets/images/image_fx.png';
   static const String kStarlightResidence =
       'assets/images/starlight_residence.png';
+  static const String kMaplewoodCottage = 'assets/images/maplewood_cottage.png';
+  static const String kGroveHomestead = 'assets/images/grove_homestead.jpg';
+  static const String kSunnydaleCottage = 'assets/images/sunnydale_cottage.jpg';
 }
