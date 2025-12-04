ea7f1ac1fdb9e1b53ab957565399e1d490c6a8ec
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-12-04 13:19:06 +0100

    Update colors.dart

--- lib/core/presentation/themes/colors.dart
+++ lib/core/presentation/themes/colors.dart
@@ -6,17 +6,22 @@ class AppColors {
   static final Color kWhite = Color(0xFFFFFFFF);
   static final Color kBlack = Color(0xFF000000);
   static final Color kGreyBg = Color(0xFFFBFDFF);
-  static final Color kGrey30 = Color(0xFF7A7A7A);
   static final Color kGray30 = Color(0xFFCBD5E1);
   static final Color kGrey5 = Color(0xFFEAEAEA);
   static final Color kBattleshipGray = Color(0xFF898989);
   static final Color kRed = Color(0xFFFF0000);
-  static final Color kGrey20 = Color(0xFFA4A4A4);
   static final Color kTransparent = Color(0x00000000);
   static final Color kEerieBlack = Color(0xFF171717);
 
+  static final Color kGrey10 = Color(0xFFCCCCCC);
+  static final Color kGrey20 = Color(0xFFA4A4A4);
+  static final Color kGrey30 = Color(0xFF7A7A7A);
   static final Color kGrey40 = Color(0xFF111111);
   static final Color kGrey70 = Color(0xFF1F1F1F);
+  static final Color kGrey80 = Color(0xFF191919);
+  static final Color kGrey90 = Color(0xFF141414);
+
+  static final Color kWarning50 = Color(0xFFF59E0B);
 
   static final Color kPlatinum = Color(0xFFEAEAEA);
   static final Color kSlimyGreen = Color(0xFF169D12);
